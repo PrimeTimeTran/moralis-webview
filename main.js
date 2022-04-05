f35fdf55525759f388482cab5e2f5ef7e2430c5b
Loi Van Tran <datloiboi@gmail.com>
2022-04-05 12:08:13 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const serverUrl = "https://xdfyrvucctvb.usemoralis.com:2053/server";
+const appId = "iahVs9hPRAj4S9Yv0U8kAII4K6Lul94Oft1BilUj";
+
+Moralis.start({ serverUrl, appId });
+
+/** Add from here down */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+    try {
+      user = await Moralis.authenticate({ signingMessage: "Hello World!" })
+      console.log(user)
+      console.log(user.get('ethAddress'))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
